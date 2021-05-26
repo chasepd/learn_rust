802d3f0ee76810cb65a4437bedc2f3f164db895c
Chase Davies <chase.davies@sproutsocial.com>
2021-05-26 16:00:28 -0600

    completed temperature converters for control flow section

--- projects/rust_book/control_flow/src/main.rs
+++ projects/rust_book/control_flow/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
     let n = 20;
-    println!("The fibonacci number at position {} is {}", n, fibonacci(n));
+    println!("\nThe fibonacci number at position {} is {}\n\n", n, fibonacci(n));
+
+    let temp = 130.0;
+    println!("{} degrees C is {} degrees F", temp, temperature_convert_c_to_f(temp));
+    println!("{} degrees C is {} degrees K", temp, temperature_convert_c_to_k(temp));
+    println!("{} degrees F is {} degrees C", temp, temperature_convert_f_to_c(temp));
+    println!("{} degrees F is {} degrees K", temp, temperature_convert_f_to_k(temp));
+    println!("{} degrees K is {} degrees C", temp, temperature_convert_k_to_c(temp));
+    println!("{} degrees K is {} degrees F", temp, temperature_convert_k_to_f(temp));
 }
 
 fn fibonacci(x : i64) -> i64{
@@ -12,7 +20,37 @@ fn fibonacci(x : i64) -> i64{
         final_num = current_number;
         two_ago = one_ago;
         one_ago = current_number;
-        println!("{}", current_number);
+        print!("{} ", current_number);
     }
     final_num
+}
+
+fn temperature_convert_c_to_f(celsius : f64) -> f64{
+    let fahrenheit = (celsius * (9.0/5.0)) + 32.0;
+    fahrenheit
+}
+
+fn temperature_convert_c_to_k(celsius : f64) -> f64{
+    let kelvin = celsius + 273.15;
+    kelvin
+}
+
+fn temperature_convert_f_to_c(fahrenheit : f64) -> f64{
+    let celsius = (fahrenheit - 32.0) / (9.0/5.0);
+    celsius
+}
+
+fn temperature_convert_f_to_k(fahrenheit : f64) -> f64{
+    let kelvin = temperature_convert_c_to_k(temperature_convert_f_to_c(fahrenheit));
+    kelvin
+}
+
+fn temperature_convert_k_to_c(kelvin : f64) -> f64{
+    let celsius = kelvin - 273.15;
+    celsius
+}
+
+fn temperature_convert_k_to_f(kelvin : f64) -> f64{
+    let fahrenheit = temperature_convert_c_to_f(temperature_convert_k_to_c(kelvin));
+    fahrenheit
 }
---
+++ projects/rust_book/control_flow/target/rls/debug/deps/control_flow-054be8875c7a8ac9.d
@@ -0,0 +1,5 @@
+/Users/chasedavies/dev/personal/learn_rust/projects/rust_book/control_flow/target/rls/debug/deps/control_flow-054be8875c7a8ac9.rmeta: src/main.rs
+
+/Users/chasedavies/dev/personal/learn_rust/projects/rust_book/control_flow/target/rls/debug/deps/control_flow-054be8875c7a8ac9.d: src/main.rs
+
+src/main.rs:
