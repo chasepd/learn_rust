43ce8289cb3f8e7342632c652f335708b3277aea
Chase Davies <chase.davies@sproutsocial.com>
2021-06-02 13:30:23 -0600

    finished ownership chapter

--- projects/rust_book/ownership/src/main.rs
+++ projects/rust_book/ownership/src/main.rs
@@ -1,5 +1,30 @@
+
 fn main() {
     let mut s = String::from("hello");
     s.push_str(", world!");
-    println!("{}", s);
+    s = takes_ownership_and_gives_back(s);
+    println!("{} {}", s, calculate_length(&s));
+    let word = first_word(&s);
+    println!("{}", word );
+}
+
+fn takes_ownership_and_gives_back(some_string: String) -> String { 
+    println!("{}", some_string);
+    some_string
+} 
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
